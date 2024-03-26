5f5d16bc153a17829623883bafc1db0292135d4b
Stephen <stephenchow1111@gmail.com>
2024-03-27 03:24:17 +0800

    fixed index.js, added table.jsx

--- src/component/DashboardContainer.jsx
+++ src/component/DashboardContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Table from './Table';
 
 function Dashboard() {
   const [data, setData] = useState([]);
@@ -18,34 +19,9 @@ function Dashboard() {
   }, []);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Function</th>
-          <th>Number of Pings</th>
-          <th>Number Of Cold Starts</th>
-          <th>Cold Start %</th>
-          <th>Average Latency</th>
-          <th>Cold Start Latency</th>
-          <th>Warm Start Latency</th>
-          <th>Cold Start Latency vs Warm</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.functionName}</td>
-            <td>{item.numberOfPings}</td>
-            <td>{item.numberOfColdStarts}</td>
-            <td>{item.coldStartPercentage}</td>
-            <td>{item.averageLatency}</td>
-            <td>{item.coldStartLatency}</td>
-            <td>{item.warmStartLatency}</td>
-            <td>{item.coldStartLatencyVsWarm}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <Table data={data} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './component/App.jsx';
 import './styles.scss';
 
-const root = createRoot(document.getElementById('contents'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <div className="App">
     <App />
