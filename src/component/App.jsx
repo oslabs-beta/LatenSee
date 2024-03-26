4677a275d85261c2e2d071a89f4e59e731b3bbab
Stephen <stephenchow1111@gmail.com>
2024-03-27 03:51:18 +0800

    added .babel.rc, ran npm build

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import DashboardContainer from './DashboardContainer';
-import GraphContainer from './GraphContainer';
+// import GraphContainer from './GraphContainer';
 
 const App = () => {
   return (
     <div>
       <div className="graph-container">
         <h1>This is a graph container</h1>
-        <GraphContrainer />
+        {/* <GraphContrainer /> */}
       </div>
       <div className="dashboard-container">
         <h1>THis is a dashboard container</h1>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './component/App.jsx';
-import './styles.scss';
+// import './styles.scss';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
