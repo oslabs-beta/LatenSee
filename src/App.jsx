41a8cb577700e28ed4e2539b4b4025ed931cac1b
Reem <abiammar.reem@gmail.com>
2024-03-26 13:11:10 -0300

    pushing server and mock app file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const App = () => {
+    return (
+        <div>
+            <h1>Testing here</h1>
+            <h1>Test again</h1>
+        </div>
+    )
+}
+
+export default App; 
