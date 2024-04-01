863f3cface06f46463688c4f93980af313422396
Reem <abiammar.reem@gmail.com>
2024-04-01 15:58:55 -0300

    data controller returns data for table

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -48,15 +48,55 @@ latency for warm starts per day and per week and overall
 // middleware that gets number of runs for each function using all data available  
 dataController.getRuns = async (req, res, next) => {
   try {
+    // get array of all the functions from previous middleware 
     const {records} = res.locals; 
+
+    // get period of calculation (day, week, all data) from queryparams HARDCODED FOR NOW - TO DISCUSS WITH STEPHEN
+    // if one day period = 1, if one week period = 7, if all data available, period = Date.now()/86400000
+    const period = Date.now()/86400000
+    //change period to milliseconds
+    const periodMS = period * 86400000
+    // calculate startDate as current date minus the period we are covering in milliseconds
+    const startDate = Date.now() - new Date(periodMS)
+    // console.log("start date", startDate)
+    const endDate = Date.now()
+
     const data = await csvFuncs.getAllRows (datafileName)
     const totalRuns = []; 
-    // for each function in the user file, calculate the number of runs between two specified dates 
-    records.forEach(row => {
+    // for each function in the user file, calculate the number of runs between two specified dates (end date is alws now, and start date can be 1 day ago, 7 days ago or 0 for all data available)
+    records.forEach(row => { 
       // count tracks number of runs for each function in specified date
-      let count = csvFuncs.getTotalRuns(data, row.funcID, 0, Date.now()); 
-      // push count into the totalRuns array with additional infoprmation about the current function
-      totalRuns.push({id: row.funcID, name:row.funcName, totalRuns: count}); // for funcid= 1 [{id: 1, name: testfuncforApp1, totalRuns=count=10, numberRun:xx, numWarm }]
+      let count = csvFuncs.getTotalRuns(data, row.funcID, startDate, endDate); 
+      // count cold tracks the number of runs for each function where 'firstRun' is true
+      let countCold = csvFuncs.getCold(data, row.funcID, '1', startDate, endDate); 
+      let countWarm = count - countCold
+
+            
+      // sumLat calculates the sum of the latency for each function
+      let sumLat = csvFuncs.getAverage(data, row.funcID, 'serverDifference',startDate, endDate, null)
+      let sumWarmLat = csvFuncs.getAverage(data, row.funcID, 'serverDifference',startDate, endDate, '')/(count-countCold)
+      let sumColdLat = csvFuncs.getAverage(data, row.funcID, 'serverDifference',startDate, endDate, '1')/countCold
+      
+      // percCold is the average number of cold starts and avLat is the average latency
+      let percCold = 0;  
+      let avLat = 0
+      
+      if (count !== 0) {
+         percCold = countCold/count
+         avLat = sumLat/count
+      }
+
+      // push all calculated values into the totalRuns array with additional infoprmation about the current function
+      totalRuns.push({id: row.funcID, 
+        name:row.funcName, 
+        totalRuns: count, 
+        coldStarts: countCold, 
+        percentCold: percCold, 
+        aveLatency: avLat,
+        coldLatency: sumColdLat ? sumColdLat : 0,
+        warmLatency: sumWarmLat ? sumWarmLat : 0,
+        coldToWarm: sumColdLat/sumWarmLat ? sumColdLat/sumWarmLat : 0,
+  }); // for funcid= 1 [{id: 1, name: testfuncforApp1, totalRuns=count=10, numberRun:xx, numWarm }]
     })
     res.locals.runs = totalRuns
     // console.log(res.locals.runs);
