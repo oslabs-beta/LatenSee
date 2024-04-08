0bea94a4f513c269b0fe3da2a92d81df74f06511
Evan Griffith <evan@evangriffith.net>
2024-04-08 12:09:01 -0700

    Installed jest & included test files validating jest is installed

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
