529045f3a50efb877eae2df15ff2a7ec25903626
Stephen <stephenchow1111@gmail.com>
2024-04-17 06:31:00 +0800

    minor updates on LatestPingsTable.test.js

--- __tests__/LatestPingsTable.test.js
+++ __tests__/LatestPingsTable.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import LatestPingsTable from '../src/components/LatestPingsTable';
 
 const mockData = [
