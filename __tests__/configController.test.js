3a05b55543190f27e436c1f063b96b04512c8e35
emmagawd <eijiogbe1@outlook.com>
2024-04-16 19:12:38 -0400

    finished config controller testing

--- __tests__/configController.test.js
+++ __tests__/configController.test.js
@@ -8,14 +8,34 @@ const configController = require('../server/controllers/configController');
 const csvFuncs = require('../server/controllers/csvFuncs');
 
 describe('configController.addNew', () => {
-  xit('The first time a new users adds a function, it creates a users csv file and saves the function to it', async () => {
+  // create a variable for the path where we expect the abc123.csv to live
+  const expectedPath = path.resolve(__dirname, '../server/storage/test123.csv');
+
+  beforeAll(() => {
+    // Ensure the file is deleted at the start of the test suite
+    if (fs.existsSync(expectedPath)) {
+      fs.unlinkSync(expectedPath);
+      console.log('File deleted successfully before starting the tests.');
+    }
+  });
+
+  afterAll(() => {
+    // Clean up by deleting the file after all tests have run
+    if (fs.existsSync(expectedPath)) {
+      fs.unlinkSync(expectedPath);
+      console.log('File deleted successfully after all tests.');
+    }
+  });
+
+  it('The first time a new users adds a function, it creates a users csv file and saves the function to it', async () => {
+    //addNew method requires req, res, and next
     const req = {};
     const res = {};
     const next = jest.fn();
 
-    // create a fake request body, with the parameters:
+    // form a fake request body, with the parameters:
     req.body = {
-      appName: 'TestApp',
+      appName: 'TestApp1',
       funcName: 'TestFunc1',
       funcUrl: 'http://testfunc1.com',
       funcFreq: '10S',
@@ -25,13 +45,9 @@ describe('configController.addNew', () => {
 
     console.log('req is now: ', req);
     // we should run the addNew method, with a reqBody for a new user's function
-    configController.addNew(req, res, next);
+    await configController.addNew(req, res, next);
 
-    // expect: a new file exists at the new user's csv
-    const expectedPath = path.resolve(
-      __dirname,
-      '../server/storage/test123.csv'
-    );
+    // expect there to exist a file at the abc123.csv path
     expect(fs.existsSync(expectedPath)).toBe(true);
 
     // get the latest rows of data in the expectedPath
@@ -57,25 +73,113 @@ describe('configController.addNew', () => {
     expect(expectedRows[0].funcID).toEqual('1');
   });
 
-  xit('Adds a new function to the existing CSV file', () => {});
+  it('Adds a new function to the existing CSV file', async () => {
+    //addNew method requires req, res, and next
+    const req = {};
+    const res = {};
+    const next = jest.fn();
 
-  xit('The newest function has an ID which is one higher than the previous function', () => {});
+    // form a fake request body, with the parameters:
+    req.body = {
+      appName: 'TestApp2',
+      funcName: 'TestFunc2',
+      funcUrl: 'http://testfunc2.com',
+      funcFreq: '1M',
+      warmerOn: 'Yes',
+      userID: 'Test123',
+    };
 
-  xit('Has only unique function IDs in the file', () => {});
+    await configController.addNew(req, res, next);
 
-  xit('Has all headers set to the correct things', () => {});
+    const expectedRows = await csvFuncs.getAllRows(expectedPath); //get all rows in file at the test123.csv path
+    expect(expectedRows.length).toBe(2);
+    for (const property in req.body) {
+      expect(expectedRows[1][property]).toEqual(req.body[property]);
+    }
+    expect(expectedRows[1].funcID).toEqual('2');
+  });
 
-  xit('If no warmerOn status is provided in the body, will default to Yes', () => {});
+  it('The newest function has an ID which is one higher than the previous function', async () => {
+    //addNew method requires req, res, and next
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    // form a fake request body, with the parameters:
+    req.body = {
+      appName: 'TestApp3',
+      funcName: 'TestFunc3',
+      funcUrl: 'http://testfunc3.com',
+      funcFreq: '10S',
+      warmerOn: 'No',
+      userID: 'Test123',
+    };
+
+    await configController.addNew(req, res, next);
+
+    const expectedRows = await csvFuncs.getAllRows(expectedPath);
+
+    // Parse IDs as integers since they are stored as strings
+    const previousFuncID = Number(expectedRows[1].funcID);
+    const newFuncID = Number(expectedRows[2].funcID);
+    console.log(previousFuncID); // 2
+    console.log(newFuncID); // 3
+
+    // Check if the new function's ID is one higher than the previous function's ID
+    expect(newFuncID).toEqual(previousFuncID + 1);
+  });
+
+  it('Has only unique function IDs in the file', async () => {
+    const expectedRows = await csvFuncs.getAllRows(expectedPath);
+
+    // Use Set, since Sets contain no duplicates
+    const seenIDs = new Set();
+
+    expectedRows.forEach((row) => {
+      expect(seenIDs.has(row.funcID)).toBe(false); // Check that the ID has not been seen before
+      seenIDs.add(row.funcID); // Add the ID to the set (whether seen before or not)
+    });
+  });
+
+  it('Has all headers set to the correct things', async () => {
+    const expectedRows = await csvFuncs.getAllRows(expectedPath);
+
+    // Assuming the first row exists and using Object.keys to get headers
+    const actualHeaders = Object.keys(expectedRows[0]);
+
+    const expectedHeaders = [
+      'funcID',
+      'appName',
+      'funcName',
+      'funcFreq',
+      'userID',
+      'warmerOn',
+      'funcUrl',
+    ];
+
+    expect(actualHeaders).toEqual(expectedHeaders);
+  });
+
+  it('If no warmerOn status is provided in the body, will default to Yes', async () => {
+    //addNew method requires req, res, and next
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    // form a fake request body, with the parameters:
+    req.body = {
+      appName: 'TestApp3',
+      funcName: 'TestFunc3',
+      funcUrl: 'http://testfunc3.com',
+      funcFreq: '10S',
+      // no warmerOn status
+      userID: 'Test123',
+    };
+
+    await configController.addNew(req, res, next);
+
+    const expectedRows = await csvFuncs.getAllRows(expectedPath);
+
+    expect(expectedRows[expectedRows.length - 1].warmerOn).toEqual('Yes');
+  });
 });
-
-/**
- * 
- *     // create a variable for the path where we expect the abc123.csv to live
-    const expectedPath = path.resolve(
-      __dirname,
-      '../server/storage/abc123.csv'
-    );
-
-    // expect there to exist a file at the abc123.csv path
-    expect(fs.existsSync(expectedPath)).toBe(true);
- */
